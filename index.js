5e673d8934f8a4b46d0143421522e4c4f57160ff
sashrika15 <sashrika.surya@gmail.com>
2020-05-04 16:55:58 +0530

    Adding data to DB from csv file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var mysql = require('mysql');
 var app = express();
+var csv = require('csv-parser');
+var fs = require('fs');
 
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -21,8 +23,10 @@ app.get('/users', (req, res) => {
     connection.query('SELECT * from users', (err, rows, fields) => {
         if(err)
         console.log(err);
-        else
-        res.send(JSON.stringify(rows));
+        else{
+        res.json(rows);
+        //res.send("Number of users = ");
+        }
     })
 });
 
@@ -34,6 +38,19 @@ app.get('/users/:id', (req, res) => {
         res.send(JSON.stringify(rows));
     })
 });
+
+fs.createReadStream('data.csv').pipe(csv())
+  .on('data', (row) => {
+    var sql = `INSERT INTO user1 VALUES ?`;
+    var values = [[row.id,row.name, row.date, row.steps, row.calories]];
+    //console.log(values);
+    connection.query(sql, [values], (err, result) => {
+        if (err) console.log(err);
+
+    });
+    //console.log(sql);
+  });
+  
 
 app.listen(3000, () => {
  console.log("Server running on port 3000");
