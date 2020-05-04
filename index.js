53a7ad9d78f3ce3bd21590b651d6a2210f0e7287
sashrika15 <sashrika.surya@gmail.com>
2020-05-04 17:15:06 +0530

    Update

--- index.js
+++ index.js
@@ -1,56 +1,10 @@
-var express = require("express");
-var mysql = require('mysql');
-var app = express();
-var csv = require('csv-parser');
-var fs = require('fs');
+const express = require("express");
+const app = express();
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Vimmi@rani07',
-    database: 'app'
-})
+//Importint routes
+const usersRoute = require('./routes/users');
 
-connection.connect(function(err) {
-    if(err) 
-    console.log(err);
-
-    console.log('Connected as id ' + connection.threadId);
-   
-});
-
-app.get('/users', (req, res) => {
-    connection.query('SELECT * from users', (err, rows, fields) => {
-        if(err)
-        console.log(err);
-        else{
-        res.json(rows);
-        //res.send("Number of users = ");
-        }
-    })
-});
-
-app.get('/users/:id', (req, res) => {
-    connection.query('SELECT name from users WHERE id = ?',[req.params.id], (err, rows, fields) => {
-        if(err)
-        console.log(err);
-        else
-        res.send(JSON.stringify(rows));
-    })
-});
-
-fs.createReadStream('data.csv').pipe(csv())
-  .on('data', (row) => {
-    var sql = `INSERT INTO user1 VALUES ?`;
-    var values = [[row.id,row.name, row.date, row.steps, row.calories]];
-    //console.log(values);
-    connection.query(sql, [values], (err, result) => {
-        if (err) console.log(err);
-
-    });
-    //console.log(sql);
-  });
-  
+app.use('/users', usersRoute);
 
 app.listen(3000, () => {
  console.log("Server running on port 3000");
