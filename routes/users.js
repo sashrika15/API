3f932764a42f9f1c300d3be1e4c43ef527443378
sashrika15 <sashrika.surya@gmail.com>
2020-06-03 17:08:38 +0530

    Added Promises + functions for modularity

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const csv = require('csv-parser');
 const path = require('path')
 const testFolder = './csv/';
+//const promise = require('promise');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -14,16 +15,16 @@ const connection = mysql.createConnection({
     database: 'app'
 })
 
-connection.connect(function(err) {
+connection.connect(err => {
     if(err) throw err;
     console.log('Connected as id ' + connection.threadId);
    
 });
 
-var query = 'CREATE TABLE IF NOT EXISTS users ( id int PRIMARY KEY, name varchar(255) )';
+var q1 = 'CREATE TABLE IF NOT EXISTS users ( id int PRIMARY KEY, name varchar(255) )';
 var q2 = 'CREATE TABLE IF NOT EXISTS user_data ( id int, date bigint, steps int, calories int )';
 
-connection.query(query, (err,result) => {
+connection.query(q1, (err,result) => {
     if (err) throw err;
 });
 
@@ -31,63 +32,89 @@ connection.query(q2, (err,result) => {
     if (err) throw err;
 });
 
-//Routes 
+function read(testFolder) {
+    return new Promise((resolve,reject) => {
+        fs.readdir(testFolder, (err, files) => {
+           resolve(files);
+            })
+        })
+    }
 
-router.post('/', (req, res) => {
-    fs.readdir(testFolder, (err, files) => {
-        if(err) throw err;
+var all_files = [];
+
+function checkCSV(files) {
+    return new Promise((resolve,reject) => {
         files.forEach(file => {
             var e = path.extname(file);
-            //console.log(e);
-            if(e=='.csv') {
-                //console.log(files);
-                fs.createReadStream(testFolder + file)
-                .on('error', (err) => {
-                    console.log(err);
-                })
+            if(e=='.csv')
+            all_files.push(file);
+            resolve(all_files);
+        })
+    })
+}
+
+
+function sql_insert(query,data) {
+    return new Promise((resolve,reject) => {
+        connection.query(query, [data], (err, result) => {
+            if (err)  throw reject(err);
+            resolve();
+        });
+    })
+}
+
+//Routes
+
+router.post('/', (req,res) => {
+    read(testFolder)
+    .then((values) => {
+        checkCSV(values)
+        .then((file) => {
+           for (i=0; i<file.length; i++)
+           {
+           fs.createReadStream(testFolder + file[i])
+                .on('error', (err) => console.log(err))
                 .pipe(csv())
                 .on('data', (row) => {
-                    var sql = `INSERT IGNORE INTO users VALUES ?`;
-                    var values = [[row.id,row.name]];
+                    var sql1 = `INSERT IGNORE INTO users VALUES ?`;
                     var sql2 = 'INSERT IGNORE INTO user_data VALUES ?';
-                    var val = [[row.id, row.date, row.steps, row.calories]];
-                    // console.log(row);
-                    connection.query(sql, [values], (err, result) => {
-                        if (err)  throw err;
-                    });
-                    connection.query(sql2, [val], (err, result) => {
-                        if (err)  throw err;
-                    });
-                // console.log(file);
+                    sql_insert(sql1, [[row.id, row.name]])
+                    .then(sql_insert(sql2, [[row.id, row.date, row.steps, row.calories]]))
+                    .catch(err => console.log(err)) 
                 })
                 .on('end', () => {
                     console.log("Reached end of csv")
                 });
             }
-        });
-    });
-});
+        })
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err));
+})
+
+function sql_query(query , data) {
+    return new Promise((resolve,reject) => {
+        connection.query(query, data, (err, rows) => {
+            resolve(rows);
+            if(err) reject(err);
+        })
+    })
+}
 
 router.get('/', (req, res) => {
-    connection.query('SELECT * from users', (err, rows) => {
-        if(err)
-            console.log(err);
-        else
-            res.json(rows);
-    }); 
+    sql_query('SELECT * from users')
+    .then((rows) => res.json(rows))
+    .catch(err => console.log(err))
 });
+
 
 router.get('/:id', (req, res) => {
-    
-    connection.query('SELECT * from users JOIN user_data WHERE users.id = ? AND user_data.id = ? ORDER BY user_data.date',
-    [req.params.id, req.params.id], (err, rows, fields) => {
-        if(err)
-            console.log(err);
-        else
-            res.json(rows);
-    });
+    var query = 'SELECT * from users JOIN user_data WHERE users.id = ? \
+    AND user_data.id = ? ORDER BY user_data.date'
+    sql_query(query,[req.params.id, req.params.id])
+    .then(rows => res.json(rows))
+    .catch(err => console.log(err))
 });
-
 
 
 
